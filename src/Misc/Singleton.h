aea2d9059c1f21f98eaf93ab14b8df2cf5f3b3a6
bartlomiejrembisz <bartlomiej_rembisz@o2.pl>
2020-03-21 16:06:02 +0000

    BartOS/Misc: Add usage instructions to the Singleton class

--- src/Misc/Singleton.h
+++ src/Misc/Singleton.h
@@ -1,6 +1,26 @@
 #ifndef SINGLETON_H
 #define SINGLETON_H
 
+/*
+ *  Usage:
+ * 
+ * class MySingleton : public Singleton<MySingleton>
+ * {
+ *  private:
+ *      //! Private constructor
+ *      MySingleton();
+ * 
+ *      friend class Singleton<MySingleton>;
+ * }
+ * 
+ * 
+ * MySingleton::GetInstance();
+ */
+
+
+/*
+ *  @brief The Singleton class.
+ */
 template <typename CLASS>
 class Singleton
 {
